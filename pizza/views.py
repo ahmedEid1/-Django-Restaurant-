2c8d237d72fd788063a40a27952b5dd75f6806a6
ahmedeid <kingmedo954@gmail.com>
2020-08-25 20:53:54 +0200

    creating order-piza form and view

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import PizzaForm
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'pizza/home.html')
+
+
+def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'thanks for ordering! your %s %s and %s pizza is on its way!' %(
+                filled_form.cleaned_data['size'],
+                filled_form.cleaned_data['topping1'],
+                filled_form.cleaned_data['topping2'])
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html',
+                          {'pizza_form': new_form,
+                           'note': note})
+
+    form = PizzaForm()
+    return render(request, 'pizza/order.html',
+                  {'pizza_form': form})
