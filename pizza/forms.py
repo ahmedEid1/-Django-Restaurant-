a1aa1e011b3ea030e4007315b6c51c3fa5dfbc95
ahmedeid <kingmedo954@gmail.com>
2020-08-26 19:26:27 +0200

    adding style using widget-tweaks & DONE

--- pizza/forms.py
+++ pizza/forms.py
@@ -11,10 +11,6 @@ from .models import Pizza, Size
 
 
 class PizzaForm(forms.ModelForm):
-
-    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None,
-                                  widget=forms.RadioSelect)
-
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
