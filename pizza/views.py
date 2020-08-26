b19d2a4ebf017ac4fd68a223c28ace348877d212
ahmedeid <kingmedo954@gmail.com>
2020-08-26 17:23:15 +0200

    creating a formset for ordering multiple  pizzas

--- pizza/views.py
+++ pizza/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import PizzaForm
+from .forms import PizzaForm, MultiplePizzaForm
+from django.forms import formset_factory
 
 
 # Create your views here.
@@ -8,6 +9,7 @@ def home(request):
 
 
 def order(request):
+    multi_form = MultiplePizzaForm()
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
@@ -18,8 +20,35 @@ def order(request):
             new_form = PizzaForm()
             return render(request, 'pizza/order.html',
                           {'pizza_form': new_form,
-                           'note': note})
+                           'note': note,
+                           'multi_form': multi_form})
 
     form = PizzaForm()
     return render(request, 'pizza/order.html',
-                  {'pizza_form': form})
+                  {'pizza_form': form, 'multi_form': multi_form})
+
+
+def pizzas(request):
+    number_of_pizzas = 2
+    filled_multi_pizza_form = MultiplePizzaForm(request.GET)
+
+    if filled_multi_pizza_form.is_valid():
+        number_of_pizzas = filled_multi_pizza_form.cleaned_data['number']
+
+    PizzaFormSet = formset_factory(PizzaForm, extra=number_of_pizzas)
+    formset = PizzaFormSet()
+
+    if request.method == "POST":
+        filled_form_set = PizzaFormSet(request.POST)
+        if filled_form_set.is_valid():
+            for form in filled_form_set:
+                print(form.cleaned_data['topping1'])
+            note = 'pizzas created successfully'
+        else:
+            note = 'something went wrong try again'
+        return render(request, 'pizza/pizzas.html',
+                      {'note': note, 'formset': formset})
+    else:
+        return render(request, 'pizza/pizzas.html',
+                      {'formset': formset})
+
